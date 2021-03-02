2dfc7e8bf3314eb2adce2344b2633cef21de6ca8
Daniel Miranda <developermiranda@gmail.com>
2021-03-02 15:38:49 -0300

    Instalando o MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#2196f3",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Button variant="contained" color="primary">
+          Fala dev
+        </Button>
+      </div>
+    </ThemeProvider>
   );
 }
 
